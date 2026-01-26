5e6e9626f0fc2828743c9d650c972819d37fa5bd
RachitJ05 <rachitjain690@gmail.com>
2026-01-27 02:36:17 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
@@ -8,10 +9,20 @@ import { functions } from "./lib/inngest.js";
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 
 app.use("/api/inngest", serve({client: inngest, functions}));
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });

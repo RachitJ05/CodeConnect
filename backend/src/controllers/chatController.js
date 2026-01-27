1858f9672e348d1690316641d0ba07b88879049d
RachitJ05 <rachitjain690@gmail.com>
2026-01-27 20:00:35 +0530

    server side auth implemented

---
+++ backend/src/controllers/chatController.js
@@ -0,0 +1,15 @@
+import { chatClient } from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+    try {
+        const token = chatClient.createToken(req.user.clerkId); // use clerkId (not mongodb _id) as it should match the ID we have in stream dashboard
+        res.status(200).json({ 
+            token ,
+            userId: req.user.clerkId,
+            userName: req.user.name,
+            userImage: req.user.profileImage
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to generate Stream token", error: error.message });
+    }
+}

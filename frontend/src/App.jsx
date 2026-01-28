7fd7474e27531d3017c397b7d7e7a82b6a3eb611
RachitJ05 <rachitjain690@gmail.com>
2026-01-28 17:59:33 +0530

    problems page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes} from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
-import { SignedIn, useUser } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react';
 import { Toaster } from 'react-hot-toast';
 import DashboardPage from './pages/DashboardPage';
 
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={!SignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
-      <Route path='/dashboard' element={SignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+      <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+      <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
       <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
     </Routes>
 

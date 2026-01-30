6004340d35fd2165e842c03cc862635dcaa2c7d4
RachitJ05 <rachitjain690@gmail.com>
2026-01-30 22:31:27 +0530

    Added Document title and svg

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,15 +29,6 @@ app.get("/health", (req, res) => {
 });
 
 
-// if (ENV.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("/{*any}", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-//   });
-// }
-
-
 const startServer = async () => {
   try {
     await connectDB();

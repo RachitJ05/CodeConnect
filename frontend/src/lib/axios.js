2fc6a867647101c2d7552f08887e602fe758cbb1
RachitJ05 <rachitjain690@gmail.com>
2026-01-30 11:40:44 +0530

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   // withCredentials: true,    // browser will send cookies to the server automatically on every single request
 });
+
 
 export default axiosInstance;
